e42358bba8e63ea26f7c1ec5364535797a1f4b67
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2024-02-25 14:20:25 +0100

    google tokenization added

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,35 @@
 import {NextResponse} from "next/server";
+import { connectMongoDB } from '@/app/lib/mongodb';
+import User from '@/app/models/user';
+import {getSession} from "next-auth/react";
 
 export async function POST(request: Request) {
-    console.log(await request.json());
-    return NextResponse.json({message: "OKAY"});
+    const { email } = await request.json();
+    try {
+        await connectMongoDB();
+
+        console.log(email);
+        if (!email) {
+            return NextResponse.json( {message: "Email is required"}, {status: 400});
+        }
+
+        let user = new User({ email: email });
+        await user.save();
+
+        return NextResponse.json({message: "User successfully created"}, {status: 200});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message: "Error during user creation"}, {status: 300});
+    }
+}
+
+export async function GET(request: Request){
+    const session = await getSession({ request } as any);
+    if (!session || !session.user) {
+        return NextResponse.json({ message: 'Unauthorized' }, {status: 400});
+    }
+
+    // Optionally, fetch more detailed user information from your database here
+    // For demonstration, we're just returning the session's user info
+    return NextResponse.json({ user: session.user }, {status: 200});
 }
