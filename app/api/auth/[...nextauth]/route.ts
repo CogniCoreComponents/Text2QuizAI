819537e8de4eb4e8c5f4991ee680f42e8f514fca
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2024-03-30 01:24:42 +0100

    vercel environments update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,7 +27,7 @@ const authOptions: NextAuthOptions = {
 
                     if (!userExists) {
                         console.log(email);
-                        const res = await fetch("http://localhost:3000/api/user", {
+                        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`, {
                             method: "POST",
                             headers: {
                                 'Content-Type': 'application/json',
